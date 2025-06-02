0d88e8813b769dff9148e5617de60b009a604e93
SangBumShon <core5630@naver.com>
2025-06-02 14:26:49 +0900

    base fastapi

---
+++ src/main/java/com/example/skaxis/repository/InterviewIntervieweeRepository.java
@@ -0,0 +1,13 @@
+package com.example.skaxis.repository;
+
+import com.example.skaxis.entity.InterviewInterviewee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface InterviewIntervieweeRepository extends JpaRepository<InterviewInterviewee, Long> {
+    
+    List<InterviewInterviewee> findByInterviewId(Long interviewId);
+}
